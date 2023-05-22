625c0335899934dd42bd7113f35336ddad56c71c
nvidiaintel <downloadchoice@gmail.com>
2023-05-22 12:41:26 +0900

    첫번째 커밋

---
+++ src/main/java/dev/mvc/gittest1/Gittest1Application.java
@@ -0,0 +1,13 @@
+package dev.mvc.gittest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gittest1Application.class, args);
+	}
+
+}
